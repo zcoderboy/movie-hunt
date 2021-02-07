b4dc2096c7c6fa8910c1e3661311da74daaf289a
Samba Ndiaye <ext_ndiaye76@orange-sonatel.com>
2021-02-07 02:20:25 +0000

    Updated header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -72,7 +72,7 @@ const Header = () => {
                 {user.email}
               </Text>
             </HStack>
-            <HStack spacing="1rem" d={bp({ base: 'none', lg: 'flex' })}>
+            <HStack spacing="1rem" d={menuDisplay}>
               <Button
                 colorScheme="primary"
                 variant="outline"
@@ -92,7 +92,7 @@ const Header = () => {
               </a>
               <Box as={FiPower} boxSize="25px" onClick={handleLogout} cursor="pointer" />
             </HStack>
-            <Box pos="relative" zIndex="1000" d={bp({ base: 'block', lg: 'none' })}>
+            <Box pos="relative" zIndex="1000" d={menuHamburger}>
               <CMenu>
                 <MenuButton
                   as={IconButton}
