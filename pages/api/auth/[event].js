e83e55c27377fe8752dc65256bb22d66429fc5a1
Samba Ndiaye <ext_ndiaye76@orange-sonatel.com>
2021-02-07 02:59:26 +0000

    Updated logout

--- pages/api/auth/[event].js
+++ pages/api/auth/[event].js
@@ -7,15 +7,19 @@ const handler = async (req, res) => {
     query: { event }
   } = req;
   if (event === 'login') {
+    supabase.auth.api.signO;
     let { data, error } = await supabase.auth.signIn(JSON.parse(req.body));
     if (error) return res.status(401).json({ error: error.message });
     res.cookie('_supabase', { ...data }, { path: '/api' });
     res.send(data);
   } else if (event === 'logout') {
-    let { error } = await supabase.auth.signOut();
-    res.cookie('_supabase', '', { maxAge: -1, path: '/api' });
-    if (error) return res.status(401).json({ error: error.message });
-    res.status(200).send('');
+    if (req.cookies['_supabase']) {
+      let session = JSON.parse(req.cookies['_supabase']);
+      let { error } = await supabase.auth.signOut(session.access_token);
+      res.cookie('_supabase', '', { maxAge: -1, path: '/api' });
+      if (error) return res.status(401).json({ error: error.message });
+      res.status(200).send('');
+    }
   } else if (event === 'register') {
     let { data, error } = await supabase.auth.signUp(JSON.parse(req.body));
     if (error) return res.status(401).json({ error: error.message });
